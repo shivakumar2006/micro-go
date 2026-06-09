8869e5e289790d5a31802498a93aeada0e41ffe3
shivakumar2006 <official.shivakumar06@gmail.com>
2026-06-09 23:46:34 +0530

    first commit

---
+++ backend/auth/cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "auth/internal/config"
+
+func main() {
+	// load config
+	config := config.LoadConfig()
+
+	// setup db
+	// add routes
+	// start server
+}
---
+++ backend/auth/internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type ServerConfig struct {
+	Addr string `yaml:"address" env-required:"true"`
+}
+
+type DBConfig struct {
+	Host     string `yaml:"host" env-required:"true"`
+	Port     int    `yaml:"port" env-required:"true"`
+	User     string `yaml:"user" env-required:"true"`
+	Password string `yaml:"password" env-required:"true"`
+	DBName   string `yaml:"dbname" env-required:"true"`
+}
+
+type JWTConfig struct {
+	Secret string `yaml:"secret" env-required:"true"`
+	Expiry string `yaml:"expiry" env-required:"true"`
+}
+
+type Config struct {
+	Env    string       `yaml:"env" env-required:"true"`
+	Server ServerConfig `yaml:"server"`
+	DB     DBConfig     `yaml:"db"`
+	JWT    JWTConfig    `yaml:"jwt"`
+}
+
+func LoadConfig() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file") // it takes name, value and usage
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+
+	// os.Stat returns the file info if info is not present means there's an error
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file doesn't exist %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Failed to read config file %s", err.Error())
+	}
+
+	log.Println("Config added successfully")
+
+	return &cfg
+}
