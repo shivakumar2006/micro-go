83102a6b4c1044f1b6d7aa83961c307204943848
shivakumar2006 <official.shivakumar06@gmail.com>
2026-06-10 19:47:05 +0530

    feat: implement graceful server shutdown with signal handling in main.go

--- backend/auth/cmd/main.go
+++ backend/auth/cmd/main.go
@@ -4,8 +4,14 @@ import (
 	"auth/internal/config"
 	"auth/internal/db"
 	"auth/internal/routes"
+	"context"
 	"log"
 	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -28,4 +34,37 @@ func main() {
 
 	router.Post("/api/v1/auth/register", routes.Register(storage))
 	// start server
+
+	server := &http.Server{
+		Addr:    config.Server.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started successfully", slog.String("address", config.Server.Addr))
+
+	// gracefully shutdown
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("failed to start server", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server successfully stopped")
 }
