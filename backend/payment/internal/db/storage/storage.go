18d3dcace05a048fbc2fb9eaf4c7fb10296f1315
shivakumar2006 <official.shivakumar06@gmail.com>
2026-08-08 23:23:28 +0530

    feat: update CreatePayment to return ID and refine error handling and update logic in payment repository

--- backend/payment/internal/db/storage/storage.go
+++ backend/payment/internal/db/storage/storage.go
@@ -9,4 +9,5 @@ type Storage interface {
 	CreatePayment(ctx context.Context, payment *models.Payment) error
 	GetPaymentByID(ctx context.Context, id int) (*models.Payment, error)
 	GetPaymentByOrderID(ctx context.Context, orderID int) (*models.Payment, error)
+	UpdatePaymentStatus(ctx context.Context, paymentID int, status string) error
 }
--- backend/payment/internal/repository/repo.go
+++ backend/payment/internal/repository/repo.go
@@ -21,10 +21,11 @@ func NewPaymentRepository(db *sql.DB) *PaymentRepository {
 }
 
 func (p *PaymentRepository) CreatePayment(ctx context.Context, payment *models.Payment) error {
-	_, err := p.Db.ExecContext(ctx, `
+	err := p.Db.QueryRowContext(ctx, `
 		INSERT INTO payments(order_id, user_id, amount, currency, provider, payment_intent_id, stripe_session_id, status, created_at, updated_at)
 		VALUES($1, $2, $3, $4, $5, $6, $7, $8, NOW(), NOW())
-	`, payment.OrderID, payment.UserID, payment.Amount, payment.Currency, payment.Provider, payment.PaymentIntentID, payment.StripeSessionID, payment.Status)
+		RETURNING id
+	`, payment.OrderID, payment.UserID, payment.Amount, payment.Currency, payment.Provider, payment.PaymentIntentID, payment.StripeSessionID, payment.Status).Scan(&payment.ID)
 
 	if err != nil {
 		var pgErr *pgconn.PgError
@@ -50,7 +51,10 @@ func (p *PaymentRepository) GetPaymentByID(ctx context.Context, id int) (*models
 	`, id).Scan(&payment.ID, &payment.OrderID, &payment.UserID, &payment.Amount, &payment.Currency, &payment.Provider, &payment.PaymentIntentID, &payment.StripeSessionID, &payment.Status, &payment.CreatedAt, &payment.UpdatedAt)
 
 	if err != nil {
-		return nil, fmt.Errorf("payment not found: %v", err)
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("payment not found")
+		}
+		return nil, fmt.Errorf("failed to get payment by id : %v", err)
 	}
 	return &payment, nil
 }
@@ -65,11 +69,33 @@ func (p *PaymentRepository) GetPaymentByOrderID(ctx context.Context, orderID int
 	`, orderID).Scan(&payment.ID, &payment.OrderID, &payment.UserID, &payment.Amount, &payment.Currency, &payment.Provider, &payment.PaymentIntentID, &payment.StripeSessionID, &payment.Status, &payment.CreatedAt, &payment.UpdatedAt)
 
 	if err != nil {
-		return nil, fmt.Errorf("payment not found: %v", err)
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("payment not found")
+		}
+		return nil, fmt.Errorf("failed to get payment by order id : %v", err)
 	}
 	return &payment, nil
 }
 
-func (p *PaymentRepository) UpdatePaymentStatus(ctx context.Context, paymentID int, status string) error {
+func (p *PaymentRepository) UpdatePaymentStatus(ctx context.Context, paymentID int, status string, paymentIntentID int) error {
+	result, err := p.Db.ExecContext(ctx, `
+		UPDATE payments
+		SET status = $2, payment_intent_id = $3, updated_at = NOW()
+		WHERE id = $1
+	`, paymentID, status, paymentIntentID)
 
+	if err != nil {
+		return fmt.Errorf("failed to update payment status : %v", err)
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get rows affected : %v", err)
+	}
+
+	if rows == 0 {
+		return fmt.Errorf("failed to update payment status : %v", sql.ErrNoRows)
+	}
+
+	return nil
 }
