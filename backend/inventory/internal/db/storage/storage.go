a4623b1f081fbe00944592db69d6e4644b8e7d47
shivakumar2006 <official.shivakumar06@gmail.com>
2026-08-10 05:08:10 +0530

    refactor: remove redundant comments from InventoryRepository interface

--- backend/inventory/internal/db/storage/storage.go
+++ backend/inventory/internal/db/storage/storage.go
@@ -6,19 +6,9 @@ import (
 )
 
 type InventoryRepository interface {
-
-	// Inventory transaction create karne ke liye
 	CreateTransaction(ctx context.Context, tx *models.Inventory) error
-
-	// Order already process hua ya nahi
 	GetTransactionByOrderID(ctx context.Context, orderID int64) (*models.Inventory, error)
-
-	// Status update
 	UpdateTransactionStatus(ctx context.Context, transactionID int64, status string) error
-
-	// By ID
 	GetTransactionByID(ctx context.Context, id int64) (*models.Inventory, error)
-
-	// (Optional) Admin / Debugging
 	GetTransactions(ctx context.Context) ([]models.Inventory, error)
 }
