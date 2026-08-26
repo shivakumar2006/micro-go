b36b0d5ed664706a1ed2a982f495e17878bd5c8f
shivakumar2006 <official.shivakumar06@gmail.com>
2026-08-26 16:33:04 +0530

    feat: implement analytics service methods and update storage interface definitions

--- backend/analytics/internal/db/storage/storage.go
+++ backend/analytics/internal/db/storage/storage.go
@@ -7,8 +7,8 @@ import (
 
 type Storage interface {
 	CreatePaymentAnalytic(ctx context.Context, event *models.Analytics) error
-	GetPaymentAnalytic(ctx context.Context) ([]models.Analytics, error)
+	GetPaymentAnalytics(ctx context.Context) ([]models.Analytics, error)
 	GetPaymentByPaymentID(ctx context.Context, id int64) (*models.Analytics, error)
-	GetPaymentAnalyticsByUserID(ctx context.Context, userID int64) ([]models.Analytics, error)
-	GetPaymentAnalyticsByOrderID(ctx context.Context, orderID int64) ([]models.Analytics, error)
+	GetPaymentAnalyticsByUserID(ctx context.Context, userID int64) (*models.Analytics, error)
+	GetPaymentAnalyticsByOrderID(ctx context.Context, orderID int64) (*models.Analytics, error)
 }
--- backend/analytics/internal/service/service.go
+++ backend/analytics/internal/service/service.go
@@ -3,7 +3,9 @@ package service
 import (
 	"analytics/internal/db/storage"
 	"analytics/internal/kafka"
+	"analytics/internal/models"
 	"context"
+	"fmt"
 )
 
 type Service struct {
@@ -17,5 +19,51 @@ func NewService(s storage.Storage) *Service {
 }
 
 func (s *Service) ProcessPaymentSuccess(ctx context.Context, event kafka.PaymentSuccessEvent) error {
+	analytics := models.Analytics{
+		PaymentID: event.PaymentID,
+		OrderID:   event.OrderID,
+		UserID:    event.UserID,
+		Email:     event.Email,
+		Status:    event.Status,
+		PaidAt:    event.CreatedAt,
+		CreatedAt: event.CreatedAt,
+	}
 
+	if err := s.storage.CreatePaymentAnalytic(ctx, &analytics); err != nil {
+		return fmt.Errorf("failed to insert payment analytic : %w", err)
+	}
+
+	return nil
+}
+
+func (s *Service) GetPaymentAnalytics(ctx context.Context) ([]models.Analytics, error) {
+	data, err := s.storage.GetPaymentAnalytics(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get payment analytics : %w", err)
+	}
+	return data, nil
+}
+
+func (s *Service) GetPaymentByPaymentID(ctx context.Context, paymentID int64) (*models.Analytics, error) {
+	data, err := s.storage.GetPaymentByPaymentID(ctx, paymentID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get payment by payment id : %w", err)
+	}
+	return data, nil
+}
+
+func (s *Service) GetPaymentByOrderID(ctx context.Context, orderID int64) (*models.Analytics, error) {
+	data, err := s.storage.GetPaymentAnalyticsByOrderID(ctx, orderID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get payment by order id : %w", err)
+	}
+	return data, nil
+}
+
+func (s *Service) GetPaymentByUserID(ctx context.Context, userID int64) (*models.Analytics, error) {
+	data, err := s.storage.GetPaymentAnalyticsByUserID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get payment by user id : %w", err)
+	}
+	return data, nil
 }
