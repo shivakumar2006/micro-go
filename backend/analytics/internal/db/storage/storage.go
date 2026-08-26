63edf374e24348a9fbdcf2929cc4c4ef026d49a0
shivakumar2006 <official.shivakumar06@gmail.com>
2026-08-26 16:02:46 +0530

    feat: add methods to query payment analytics by user and order ID in storage interface

--- backend/analytics/internal/db/storage/storage.go
+++ backend/analytics/internal/db/storage/storage.go
@@ -9,4 +9,6 @@ type Storage interface {
 	CreatePaymentAnalytic(ctx context.Context, event *models.Analytics) error
 	GetPaymentAnalytic(ctx context.Context) ([]models.Analytics, error)
 	GetPaymentByPaymentID(ctx context.Context, id int64) (*models.Analytics, error)
+	GetPaymentAnalyticsByUserID(ctx context.Context, userID int64) ([]models.Analytics, error)
+	GetPaymentAnalyticsByOrderID(ctx context.Context, orderID int64) ([]models.Analytics, error)
 }
